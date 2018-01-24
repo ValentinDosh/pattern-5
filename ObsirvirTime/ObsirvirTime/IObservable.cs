197a64b332488682c80f3ab0bf7fd8bd5e4a7f66
Valentin <harrypotter9173656@gmail.com>
2018-01-24 07:30:16 +0300

    Task3 for Checking

---
+++ ObsirvirTime/ObsirvirTime/IObservable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObsirvirTime
+{
+    interface IObservable
+    {
+        void AddObsirvir(IObserver o);
+        void RemoveObsirvir(IObserver o);
+        void NotifyObservers();
+    }
+}
---
+++ ObsirvirTime/ObsirvirTime/TimiUpdate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ObsirvirTime
+{
+    class TimiUpdate
+    {
+        IObservable _a;
+        
+        public TimiUpdate(IObservable a)
+        {
+            _a = a;
+        }
+
+        // метод будет менять состояние каждые 5 секунд
+        public void Change()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(5000);
+                ((A)_a).Change();
+                Console.WriteLine("{0} запись из 5", i + 1);
+            }
+            
+        }
+    }
+}
