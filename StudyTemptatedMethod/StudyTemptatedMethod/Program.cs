fd1d03266279e2d94996af1d79b265b00d2e11c3
Valentin <harrypotter9173656@gmail.com>
2018-01-24 07:24:44 +0300

    Task1 for Cheking

---
+++ StudyTemptatedMethod/StudyTemptatedMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyTemptatedMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractStud school = new School();
+            AbstractStud univer = new Univrcity();
+
+            school.Studing();
+            univer.Studing();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StudyTemptatedMethod/StudyTemptatedMethod/School.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyTemptatedMethod
+{
+    class School : AbstractStud
+    {
+        public override void Enter()
+        {
+            Console.WriteLine("Дети идут в первый класс");
+        }
+
+        public override void Study()
+        {
+            Console.WriteLine("Ходим на уроки, делаем зарядку утром под музыку, сдаем домашку");
+        }
+
+        public override void PassExams()
+        {
+            Console.WriteLine("Сдаем Гос. Экзамены");
+        }
+
+        public override void GetAttestat()
+        {
+            Console.WriteLine("Получаем атестат");
+        }
+
+       
+
+        
+    }
+}
